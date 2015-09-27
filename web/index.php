b3919229499d42bdde110833eceba5d58ee94468
Jeremie J. Jarosh <jeremie@jarosh.org>
2015-09-27 14:31:08 -0500

    Initial Commit

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = Spark\Application::boot();
+
+$app->setMiddleware([
+    'Relay\Middleware\ResponseSender',
+    'Spark\Handler\ExceptionHandler',
+    'Spark\Handler\RouteHandler',
+    'Spark\Handler\ActionHandler',
+]);
+
+$app->addRoutes(function(Spark\Router $r) {
+    $r->get('/hello[/{name}]', 'Spark\Project\Domain\Hello');
+});
+
+$app->run();
