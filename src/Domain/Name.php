a322f16a37c6866b3bbceb928a85699886c281f1
Jeremie J. Jarosh <jeremie@jarosh.org>
2015-12-10 17:15:50 -0600

    Require Authentication to Update 'my_name' Config Variable

--- src/Domain/Name.php
+++ src/Domain/Name.php
@@ -4,18 +4,23 @@ namespace trejeraos\SparkTest\Domain;
 use Spark\Adr\DomainInterface;
 use Spark\Payload;
 use trejeraos\SparkTest\Configuration;
+use trejeraos\SparkTest\Middleware\SimpleAuth;
 
 class Name implements DomainInterface
 {
     protected $config;
+    protected $auth;
 
-    public function __construct(Configuration $config)
+    public function __construct(Configuration $config, SimpleAuth $auth)
     {
         $this->config = $config;
+        $this->auth = $auth;
+        $this->auth->authenticate();
     }
 
     public function __invoke(array $input)
     {
+
         $name = $this->config->getMyName();
 
         if (!empty($input['name'])) {
